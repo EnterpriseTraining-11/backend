9a592630900525545e332ebf4490595c3d040bf8
hieheihei <270985384@qq.com>
2020-07-28 18:17:17 +0800

    merge done

--- src/main/java/com/github/hieheihei/hotel/room/controller/RoomController.java
+++ src/main/java/com/github/hieheihei/hotel/room/controller/RoomController.java
@@ -6,50 +6,45 @@ import com.github.hieheihei.hotel.room.service.IRoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/room")
 public class RoomController {
-
-    private IRoomService roomService;
+    private final IRoomService roomService;
 
     @Autowired
     public RoomController(IRoomService roomService) {
         this.roomService = roomService;
     }
 
-    @PostMapping("/add")
-    public Result<RoomModel> add(AddFormData data) {
-        roomService.add(data.code, data.type);
-
+    @CrossOrigin
+    @GetMapping(value = "/all-with-type")
+    public Result<RoomModel> getByAllWithType() {
         Result<RoomModel> result = new Result<>();
+        result.setModels(roomService.getByAllWithType());
         result.setStatus("OK");
-        result.setMessage("成功添加房间：" + data.code);
+        result.setMessage("查询成功");
         return result;
     }
 
-    @RequestMapping("/delete")
-    public Result<RoomModel> delete(@RequestParam int id) {
-        return null;
-    }
-
-    @GetMapping("/list/{page}")
-    public Result<RoomModel> getListWithPage(@RequestParam int rows, @PathVariable int page) {
-
-        List<RoomModel> list = roomService.getListWithPage(rows, page);
+    @CrossOrigin
+    @RequestMapping(value = "/delete")
+    public Result<RoomModel> delete(@RequestBody Map<String, Object> payLoad) {
         Result<RoomModel> result = new Result<>();
-        result.setModels(list);
+//        roomService.delete(new RoomModel().setId((Integer) payLoad.get("id")));
         result.setStatus("OK");
-        result.setMessage("成功获取房间列表,行数=" + rows + ",页号=" + page);
-
+        result.setMessage("删除成功--服务层未完成");
         return result;
     }
 
-
-}
-
-class AddFormData {
-    public String code;
-    public String type;
+    @CrossOrigin
+    @RequestMapping(value = "/add")
+    public Result<RoomModel> add(@RequestBody RoomModel rm) {
+        Result<RoomModel> result = new Result<>();
+        roomService.add(rm);
+        result.setStatus("OK");
+        result.setMessage("添加成功");
+        return result;
+    }
 }
