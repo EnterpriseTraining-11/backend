74174e30164aee042fe6499b75137fa747e4fa88
Laurence042 <qq2120286535@gmail.com>
2020-07-28 19:39:41 +0800

    add hint on api, remove redundant RoomController

--- src/main/java/com/github/hieheihei/hotel/room/controller/RoomController.java
+++ src/main/java/com/github/hieheihei/hotel/room/controller/RoomController.java
@@ -18,8 +18,12 @@ public class RoomController {
         this.roomService = roomService;
     }
 
+    /**
+     * param:
+     * @return
+     */
     @CrossOrigin
-    @GetMapping(value = "/all-with-type")
+    @GetMapping(value = "/query/all")
     public Result<RoomModel> getByAllWithType() {
         Result<RoomModel> result = new Result<>();
         result.setModels(roomService.getByAllWithType());
@@ -28,23 +32,78 @@ public class RoomController {
         return result;
     }
 
+    /**
+     * param: name
+     * @return
+     */
     @CrossOrigin
-    @RequestMapping(value = "/delete")
-    public Result<RoomModel> delete(@RequestBody Map<String, Object> payLoad) {
+    @GetMapping(value = "/query/name")
+    public Result<RoomModel> getByNameWithType(@RequestBody RoomModel rm) {
         Result<RoomModel> result = new Result<>();
-//        roomService.delete(new RoomModel().setId((Integer) payLoad.get("id")));
+//        result.setModels(roomService.getByAllWithType());
+        result.setStatus("OK");
+        result.setMessage("查询成功");
+        return result;
+    }
+
+    /**
+     * param: type.id
+     * @param rm
+     * @return
+     */
+    @CrossOrigin
+    @GetMapping(value = "/query/type")
+    public Result<RoomModel> getByTypeWithType(@RequestBody RoomModel rm) {
+        Result<RoomModel> result = new Result<>();
+//        result.setModels(roomService.getByAllWithType());
+        result.setStatus("OK");
+        result.setMessage("查询成功");
+        return result;
+    }
+
+    /**
+     * payload: id
+     * @param rm
+     * @return
+     */
+    @CrossOrigin
+    @PostMapping(value = "/delete")
+    public Result<RoomModel> delete(@RequestBody RoomModel rm) {
+        Result<RoomModel> result = new Result<>();
+        roomService.delete(rm);
         result.setStatus("OK");
         result.setMessage("删除成功--服务层未完成");
         return result;
     }
 
+    /**
+     * payload: code, name, type.id
+     * @param rm
+     * @return
+     */
     @CrossOrigin
-    @RequestMapping(value = "/add")
+    @PostMapping(value = "/add")
     public Result<RoomModel> add(@RequestBody RoomModel rm) {
         Result<RoomModel> result = new Result<>();
         roomService.add(rm);
         result.setStatus("OK");
         result.setMessage("添加成功");
+        return result;
+    }
+
+    /**
+     * payload: id, code, name, type.id
+     * id必有，其他三项可选，只修改不为null的属性
+     * @param rm
+     * @return
+     */
+    @CrossOrigin
+    @PostMapping(value = "/modify")
+    public Result<RoomModel> modify(@RequestBody RoomModel rm) {
+        Result<RoomModel> result = new Result<>();
+//        roomService.modify(rm);
+        result.setStatus("OK");
+        result.setMessage("修改成功");
         return result;
     }
 }
